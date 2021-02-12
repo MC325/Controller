7e41b32b35c1d74b628d22a357e1f31eeeef0582
mc325 <mchen01@bvsd.org>
2021-02-11 21:33:22 -0700

    Added constructor, setSetPoint, and getControlValue methods

--- src/main/java/PIDController.java
+++ src/main/java/PIDController.java
@@ -1,5 +1,25 @@
 public class PIDController {
+    double sensorValue;
+    double setPoint;
+
     public String hello () {
         return "Hello world!";
+    }
+
+    public PIDController (double sensorValue) {
+        if (sensorValue < -1.0) {
+            sensorValue = -1.0;
+        } else if (sensorValue > 1.0) {
+            sensorValue = 1.0;
+        }
+        this.sensorValue = sensorValue;
+    }
+
+    public void setSetPoint(double setPoint) {
+        this.setPoint = setPoint;
+    }
+
+    public double getControlValue() {
+        return (setPoint - sensorValue)/2.0;
     }
 }
--- src/test/java/PIDControllerTest.java
+++ src/test/java/PIDControllerTest.java
@@ -5,7 +5,14 @@ import static org.junit.Assert.*;
 public class PIDControllerTest {
     @Test
     public void helloTest () {
-        PIDController controller = new PIDController();
+        PIDController controller = new PIDController(1);
         assertEquals("Hello world!", controller.hello());
+    }
+
+    @Test
+    public void ProportionalTest () {
+        PIDController controller = new PIDController(1);
+        controller.setSetPoint(0.3);
+        assertEquals(-0.35, controller.getControlValue(), 0.00001);
     }
 }
