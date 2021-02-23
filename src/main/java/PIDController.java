4f84538a9ef9868d50a770c6d202d3058789e0d8
mc325 <mchen01@bvsd.org>
2021-02-22 19:55:15 -0700

    Added integral value without integral windup control

--- src/main/java/PIDController.java
+++ src/main/java/PIDController.java
@@ -2,6 +2,9 @@ public class PIDController {
     double sensorValue;
     double setPoint;
 
+    double integral = 0;
+    double previousTimeMs;
+
     public PIDController() {
     }
 
@@ -9,18 +12,37 @@ public class PIDController {
         this.setPoint = setPoint;
     }
 
-    private void setSensorValue(double sensorValue) {
-        //Sets prevents sensorValue from going over or under the maximum/minimum value
-        if (sensorValue < -1.0) {
-            sensorValue = -1.0;
-        } else if (sensorValue > 1.0) {
-            sensorValue = 1.0;
-        }
-        this.sensorValue = sensorValue;
+//    private void setSensorValue(double sensorValue) {
+//        //Sets prevents sensorValue from going over or under the maximum/minimum value
+//        bounds(-1.0,1.0,sensorValue);
+//        this.sensorValue = sensorValue;
+//    }
+
+    public double updateControlValue(double sensorValue, double currentTimeMs) {
+        this.sensorValue = bounds(-1.0, 1.0, sensorValue);
+
+        double elapsedTimeSec = (currentTimeMs - previousTimeMs) / 1000;
+        double error = setPoint - this.sensorValue;
+        integral += error * elapsedTimeSec;
+        previousTimeMs = currentTimeMs;
+
+        double proportional = error / 2.0;
+
+        double controlValue = proportional + integral;
+
+        return bounds(-1.0, 1.0, controlValue);
     }
 
-    public double updateControlValue(double sensorValue) {
-        setSensorValue(sensorValue);
-        return (setPoint - this.sensorValue) / 2.0;
+    public double getIntegral() {
+        return integral;
+    }
+
+    private static double bounds(double min, double max, double value) {
+        if (value < min) {
+            value = min;
+        } else if (value > max) {
+            value = max;
+        }
+        return value;
     }
 }
--- src/test/java/PIDControllerTest.java
+++ src/test/java/PIDControllerTest.java
@@ -4,37 +4,53 @@ import static org.junit.Assert.*;
 
 public class PIDControllerTest {
     @Test
-    public void ProportionalTest () {
+    public void proportionalTest () {
         PIDController controller = new PIDController();
 
         //Moves to the left, negative speed value
         controller.setSetPoint(-0.3);
-        assertEquals(-0.3, controller.updateControlValue(0.3), 0.00001);
+        assertEquals(-0.3, controller.updateControlValue(0.3, 0), 0.00001);
 
         //Moves to the same location, no value
         controller.setSetPoint(0.3);
-        assertEquals(0, controller.updateControlValue(0.3), 0.00001);
+        assertEquals(0, controller.updateControlValue(0.3, 0), 0.00001);
 
         //Moves to the right, positive speed value
         controller.setSetPoint(0.6);
-        assertEquals(0.15, controller.updateControlValue(0.3), 0.00001);
+        assertEquals(0.15, controller.updateControlValue(0.3, 0), 0.00001);
 
         //Movement test
         controller.setSetPoint(-0.3);
 
-        assertEquals(-0.3, controller.updateControlValue(0.3), 0.00001);
+        assertEquals(-0.3, controller.updateControlValue(0.3,0), 0.00001);
 
-        assertEquals(-0.15, controller.updateControlValue(0), 0.00001);
+        assertEquals(-0.15, controller.updateControlValue(0,0), 0.00001);
 
-        assertEquals(-0.075, controller.updateControlValue(-0.15), 0.00001);
+        assertEquals(-0.075, controller.updateControlValue(-0.15,0), 0.00001);
 
         //Greater than 1
         controller.setSetPoint(0);
 
-        assertEquals(-0.5, controller.updateControlValue(1.5), 0.00001);
+        assertEquals(-0.5, controller.updateControlValue(1.5,0), 0.00001);
 
         //Less than -1
 
-        assertEquals(0.5, controller.updateControlValue(-1.5), 0.00001);
+        assertEquals(0.5, controller.updateControlValue(-1.5,0), 0.00001);
+    }
+
+    @Test
+    public void integralTest () {
+        PIDController controller = new PIDController();
+
+        controller.setSetPoint(1);
+
+        //Integral proportional to time (fixed sensorValue)
+        assertEquals(0.5, controller.updateControlValue(0, 0), 0.00001);
+
+        assertEquals(0.53, controller.updateControlValue(0, 30), 0.00001);
+
+        assertEquals(1.0, controller.updateControlValue(0, 10000), 0.00001);
+
+        assertEquals(10.0, controller.getIntegral(),0.00001);
     }
 }
