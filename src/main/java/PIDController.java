3720ac7855d270180ebd612c235168cfd9e06729
mc325 <mchen01@bvsd.org>
2021-02-23 21:50:00 -0700

    Added checks for integral windup and variables for gain

--- src/main/java/PIDController.java
+++ src/main/java/PIDController.java
@@ -2,30 +2,56 @@ public class PIDController {
     double sensorValue;
     double setPoint;
 
-    double integral = 0;
+    double integralTotal = 0;
     double previousTimeMs;
+
+    //Gain values: change to increase/decrease the influence each part of the PID controller has on the output
+    double proportionalGain = 1.0;
+    double integralGain = 1.0;
+    double derivativeGain = 1.0;
 
     public void setSetPoint(double setPoint) {
         this.setPoint = setPoint;
     }
 
     public double updateControlValue(double sensorValue, double currentTimeMs) {
+
+        //Bounds the recieved sensorValue to between -1.0 and 1.0
         this.sensorValue = bounds(-1.0, 1.0, sensorValue);
 
-        double elapsedTimeSec = (currentTimeMs - previousTimeMs) / 1000;
+        //The error term: the difference between the setPoint and the current position (sensorValue)
         double error = setPoint - this.sensorValue;
-        integral += error * elapsedTimeSec;
+
+        //The proportional component: the maximum speed from the proportional component is 1.0 * proportionalGain
+        double proportional = error / 2.0;
+        proportional *= proportionalGain;
+
+        //The integral component: gets the change in time in seconds, multiplies it by the error (to scale the value), and adds it to the running total of the integral.
+        double elapsedTimeSec = (currentTimeMs - previousTimeMs) / 1000;
+        integralTotal += error * elapsedTimeSec;
+        double integral = integralTotal * integralGain;
         previousTimeMs = currentTimeMs;
 
-        double proportional = error / 2.0;
-
+        //Sums up all components of the PID controller and outputs the bounded value
         double controlValue = proportional + integral;
+
+        //Running checks for the integral component to see if:
+        //A) The value is saturating
+        //B) The total integral and the added integral are the same sign
+        if (Math.abs(controlValue) > 1.0 && Math.signum(error) == Math.signum(controlValue)) {
+            if (Math.signum(controlValue) < 0) {
+                controlValue = -1.0;
+            } else {
+                controlValue = 1.0;
+            }
+            integralTotal = controlValue - (proportional);
+        }
 
         return bounds(-1.0, 1.0, controlValue);
     }
 
     public double getIntegral() {
-        return integral;
+        return integralTotal;
     }
 
     private static double bounds(double min, double max, double value) {
--- src/test/java/PIDControllerTest.java
+++ src/test/java/PIDControllerTest.java
@@ -45,12 +45,26 @@ public class PIDControllerTest {
         controller.setSetPoint(1);
 
         //Integral proportional to time (fixed sensorValue)
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(controller.updateControlValue(0,i * 30));
+            System.out.println(controller.getIntegral());
+        }
+
         assertEquals(0.5, controller.updateControlValue(0, 0), 0.00001);
+
+        assertEquals(0.0, controller.getIntegral(),0.00001);
 
         assertEquals(0.53, controller.updateControlValue(0, 30), 0.00001);
 
-        assertEquals(1.0, controller.updateControlValue(0, 10000), 0.00001);
+        assertEquals(0.03, controller.getIntegral(),0.00001);
 
-        assertEquals(10.0, controller.getIntegral(),0.00001);
+        assertEquals(0.53, controller.updateControlValue(0, 10000), 0.00001);
+
+        assertEquals(0.0299999, controller.getIntegral(),0.00001);
+
+        assertEquals(0.530999, controller.updateControlValue(0, 10001), 0.00001);
+
+        assertEquals(0.03099, controller.getIntegral(),0.00001);
     }
 }
